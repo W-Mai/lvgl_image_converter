0135fc862c463599993125caae53652b96a714e5
W-Mai <1341398182@qq.com>
2021-02-18 00:02:06 +0800

    添加测试函数
    其实没啥用 就玩一下

--- main_test.py
+++ main_test.py
@@ -1,8 +1,12 @@
 from lvgl_Converter import Converter
 
+
+def test_main():
+    assert 1
+
+
 if __name__ == '__main__':
     f = r"E:\USERDATAS\Pictures\pig.png"
 
     c = Converter(f, "pig", True, Converter.FLAG.CF_TRUE_COLOR_332)
     c.convert(Converter.FLAG.CF_TRUE_COLOR_332)
-
